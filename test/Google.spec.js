2e235343eef2586728e37f320f0277f3009931fd
Unknown <ALostBoyFromNeverland@outlook.com>
2018-03-15 14:41:30 -0500

    Test improvement

---
+++ test/Google.spec.js
@@ -0,0 +1,11 @@
+describe('Gicen a SDET learning protractor', () => {
+    describe('when open Google Page', () => {
+        beforeEach(() => {
+            browser.get('http://www.google.com');
+        });
+
+        it('then should have a title', () => {
+            expect(browser.getTitle()).toEqual('Google');
+        });
+    });
+});
